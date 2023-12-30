eebb76c137ec66dd0269340d21b6fa9f73e7fead
alpha-thulz <tj.tyaphile@gmail.com>
2023-12-30 06:52:16 +0200

    Setting up client

--- Server/src/main/java/za/co/tyaphile/ECommerceServer.java
+++ Server/src/main/java/za/co/tyaphile/ECommerceServer.java
@@ -44,9 +44,9 @@ public class ECommerceServer {
     }
 
     public ECommerceServer() {
-        init();
-
         new DatabaseManager();
+        createMockProducts();  // Comment out to disable creating mock items
+        init();
 
         server.post("/product", this::addProduct);
         server.post("/remove-products", this::deleteProduct);
@@ -187,5 +187,60 @@ public class ECommerceServer {
     public static void main(String[] args) {
         ECommerceServer server = new ECommerceServer();
         server.start();
+    }
+
+    private void createMockProducts() {
+        Map<String, List<Object>> products = new HashMap<>();
+        products.put("Designer Concepts Yves Plasma Stand - White 2.2m",
+                new ArrayList<>(List.of("The Yves Plasma Stand Brings a Light " +
+                "Concept To Your Living Room Through Its Straight Lines And Soft Colors", 6999)));
+        products.put("OPPO Find N2 Flip 5G 256GB Dual Sim - Moonlit Purple",
+                new ArrayList<>(List.of("OPPO Find N2 Flip 5G\n" +
+                        "Discover the tech-savvy, style statement phone that lets you see more in a snap. " +
+                        "The larger cover screen expands your view with innovative features, whilst the foldable design" +
+                        " reimagines photography possibilities, all in a gorgeous and flawless design that " +
+                        "you can pop in your pocket.", 23999)));
+        products.put("Volkano SA Travel Plug to UK Plug Traveller Series",
+                new ArrayList<>(List.of("There's nothing worse than arriving in a country with different sockets to " +
+                        "the Type-M plug standard. Prepare in advance with this handy plug converter. Note, this " +
+                        "adapter does not convert voltage, so make sure you use it with devices that have a " +
+                        "universal input voltage range.", 149)));
+        products.put("NIVEA Radiant & Beauty",
+                new ArrayList<>(List.of("NIVEA Radiant & Beauty Even Glow Body Cream with 95% Pure Vitamin C, 400ml", 159)));
+        products.put("GARDENA Garden Shower Solo", new ArrayList<>(List.of("Extends to maximum 207 cm in height\n" +
+                "Height adjustable lever\n" +
+                "Gentle soft spray pattern\n" +
+                "Integrated ground spike for easy installation", 369)));
+        products.put("GARDENA Rain Water Tank Pump 4700/2 inox", new ArrayList<>(List.of("40 yrs of innovation\n" +
+                "Reliable engineering\n" +
+                "Global market leader\n" +
+                "Safe and reliable\n" +
+                "High quality materials\n" +
+                "Convenient\n" +
+                "Versatile\n" +
+                "Eco Friendly", 2199)));
+        products.put("Everfurn Work Desk - Anthony Series",
+                new ArrayList<>(List.of("A stunningly comprised work desk that boasts ample space with " +
+                        "additional storage space. Sturdy and durable, the Anthony Series is also a looker with " +
+                        "clean lines and design precision.", 1699)));
+        products.put("Brother DCP-T720DW Ink Tank Printer 3in1 with WiFi and ADF",
+                new ArrayList<>(List.of("Specifications:\n" +
+                        "- Functions: Print, Copy, Scan with Auto two-sided print\n" +
+                        "- Connectivity: USB, Wireless\n" +
+                        "- Print speed: Up to 17/16.5 ipm (ISO)\n" +
+                        "- Paper Input: 150 Sheets and 20 sheet Auto Document Feeder,\n" +
+                        "- Single Sheet manual feed slot (max paper gsm - 300gsm)", 5499)));
+        products.put("Brother BT5000 / 6000 Combo Ink Bottle Set", new ArrayList<>(List.of("Compatible Printers\n" +
+                "\n" +
+                "Brother DCP-T300\n" +
+                "Brother DCP-T500W\n" +
+                "Brother DCP-T700W\n" +
+                "Brother MFC-T800W\n" +
+                "Black - 115ml/Colours - 45ml\n" +
+                "Page Yield - 4000/5 000 pgs @ 5% coverage\n" +
+                "High quality printing and page yields equal to Original", 154)));
+        products.put("Sugar", new ArrayList<>(List.of("1 kg of sweetener", 24.99)));
+
+        products.forEach((key, info) -> DatabaseManager.addProduct(key, info.get(0).toString(), Double.parseDouble(info.get(1).toString())));
     }
 }
